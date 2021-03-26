d7ba7670731840fb34498f97e5d059062d71ac31
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-26 16:39:23 -0500

    fix selected option in edit employee

--- app/models/employee_area.rb
+++ app/models/employee_area.rb
@@ -2,7 +2,7 @@ class EmployeeArea
   AREAS = %w(accounting finance operations security human_resources)
 
   def self.options
-    EmployeeArea::AREAS.each_with_index.map{|area, index| [area.capitalize, index]}
+    EmployeeArea::AREAS.each_with_index.map{|area, index| [area.capitalize.gsub("_"," "), index]}
   end
 
   def initialize(area)
